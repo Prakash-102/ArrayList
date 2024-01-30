1e8292ccb9cb248ce51418d16abe769db5547f9e
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-30 11:19:22 +0530

    Add files via upload

---
+++ Water.java
@@ -0,0 +1,38 @@
+
+//Container with most water?:-Flipkart
+import java.util.*;
+
+public class Water {
+
+    public static int Storwater(ArrayList<Integer> height) {
+        int maxwater = 0;
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+                int ht = Math.min(height.get(i), height.get(j));
+                int width = j - 1;
+                int currwater = ht * width;
+                maxwater = Math.max(maxwater, currwater);
+            }
+        }
+        return maxwater;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println(Storwater(height));
+    }
+}
+
+/*
+ * Output:-
+ * 49
+ */
